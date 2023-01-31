46e059b035865ac2cd81bdba2c859011ec789d5c
Christian Garcia <bones192nd@gmail.com>
2023-01-31 10:53:59 -0500

    lab

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-function addingEventListener() {
-}
+
+const addingEventListener = () => document.getElementById('button').addEventListener('click', () => alert("I was clicked"))
+
+addingEventListener()
